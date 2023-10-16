0c5465e5eec6b5224fc029d23664cbcdb033afc6
Dawid Mrotek <dawidmrotek@protonmail.ch>
2023-10-16 16:34:20 +0000

    Finish database optimization

--- blango/settings.py
+++ blango/settings.py
@@ -80,6 +80,8 @@ class Dev(Configuration):
     CSRF_COOKIE_SAMESITE = 'None'
     SESSION_COOKIE_SAMESITE = 'None'
 
+    INTERNAL_IPS = ["192.168.10.226"]
+
     # Application definition
 
     INSTALLED_APPS = [
@@ -92,9 +94,11 @@ class Dev(Configuration):
         'blog',
         'crispy_forms',
         'crispy_bootstrap5',
+        'debug_toolbar',
     ]
 
     MIDDLEWARE = [
+        'debug_toolbar.middleware.DebugToolbarMiddleware',
         'django.middleware.security.SecurityMiddleware',
         'django.contrib.sessions.middleware.SessionMiddleware',
         'django.middleware.common.CommonMiddleware',
@@ -102,7 +106,8 @@ class Dev(Configuration):
         'django.contrib.auth.middleware.AuthenticationMiddleware',
         'django.contrib.messages.middleware.MessageMiddleware',
     #     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    ]
+        
+]
 
     ROOT_URLCONF = 'blango.urls'
 
